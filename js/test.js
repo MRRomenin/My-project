8d5484b1ed791775cd2f381894013aba8001e870
MRRomenin <roma_sozontov@mail.ru>
2025-03-30 02:40:40 +0600

    mini refactor and add favicon.ico

---
+++ js/test.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+   var content = '<p>Это скрытый контент, который появляется только через JavaScript!</p>';
+   document.getElementById('hiddenContent').innerHTML = content;
+};
